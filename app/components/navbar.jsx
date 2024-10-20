cf9206b2f687739114409f5e781984dba1b195a2
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-10-20 13:33:33 +0530

    Feat: Added sliding text in the navbar and removed the nav items.

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,8 +1,9 @@
 // @flow strict
 import Link from "next/link";
-
+import './navbar.css'
 
 function Navbar() {
+  
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
@@ -13,26 +14,11 @@ function Navbar() {
             Chaitanya N K
           </Link>
         </div>
-
-        <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
-            </Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
-          </li>
-        </ul>
+        <div className="sliding-txt-container">
+          <p className="sliding-txt">
+            .NET Full Stack Developer | MERN/MEAN Stack Developer | Android App Developer | C and Python Programmer | Data Science Enthusiast
+          </p>
+        </div>
       </div>
     </nav>
   );
