131142683cb175aa43b790f97814c89c659d1149
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-11-26 21:30:04 +0530

    Feat: Added new experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
-    title: "Full Stack Developer",
-    company: "Cognizant Technology Solutions",
-    duration: "(September 2023 - Present)"
+    title: "Associate Release Engineer",
+    company: "Veeva Systems",
+    duration: "(November 2024 - Present)"
   },
   {
     id: 2,
+    title: "Full Stack Developer",
+    company: "Cognizant Technology Solutions",
+    duration: "(September 2023 - November 2024)"
+  },
+  {
+    id: 3,
     title: "Programmer Analyst Trainee Intern",
     company: "Cognizant Technology Solutions",
     duration: "(Feb 2023 - June 2023)"
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -42,7 +42,27 @@ export const projectsData = [
         code: '',
         demo: '',
         image: travel
-    }
+    },
+    // {
+    //     id: 5,
+    //     name: 'Taskify',
+    //     description: 'Taskify is a real world task management application built using Next JS. Here, we can create workspaces / organizations, we can create boards (limit to 5 for a free version). Pro version upgrades is also available to create more than 5 boards. It has features to create tasks, update them according to the status, and also other major actions. Each board also has an option to set image for it, these images are randomly generated from the unsplash API.',
+    //     tools: ['Next JS', 'React', 'Prisma', 'Docker', 'PostgreSQL'],
+    //     role: 'Full Stack Developer',
+    //     code: '',
+    //     demo: '',
+    //     image: travel
+    // },
+    // {
+    //     id: 6,
+    //     name: 'Ecommerce Admin SaaS Web Application',
+    //     description: 'Twitter live stream data is retrieved in the form of tweets using the python package snscrape, then it is stored in an excel format. The types of preprocessing techniques include removal of any urls, emails, removing the retweets to reduce redundancy in tweets, removing the html tags or any mentions in the tweet, and also correcting the spelling mistakes if there are any, in the tweet. The preprocessed dataset is trained using three different machine learning algorithms for classifying the tweets into disaster related and not related. The result of classification algorithm whose testing accuracy is high is sent for sentiment analysis to know whether the tweets are negative, positive or neutral. Finally, the tweets are given ranking using Count Vectorizer and Latent Dirichlet Allocation and the ranked tweets are displayed on a webpage.',
+    //     tools: ['Python', 'Machine Learning', 'Streamlit', 'Data Analysis', 'Sentiment Analysis'],
+    //     role: 'Full Stack Developer',
+    //     code: '',
+    //     demo: '',
+    //     image: travel
+    // }
 ];
 
 
